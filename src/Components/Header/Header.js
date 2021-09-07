ca6fe5023e3c5239113ea4e62010651e19164abf
Emilia <m.emilia.guadalupe@gmail.com>
2021-09-07 08:35:26 -0300

    Changed the text

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,7 +5,7 @@ function Header() {
     return(
         <div className="header-container">
             <h1 className="main-title">Summa Solutions FED Test</h1>
-            <p className="main-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            <p className="main-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </div>
     )
 }
