b0b08d22f6ec15101a77b84328c4c407000270ad
Emilia <m.emilia.guadalupe@gmail.com>
2021-09-07 20:28:19 -0300

    New name

---
+++ src/Components/Main/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './mainStyles.scss';
+
+function Header() {
+    return(
+        <div className="main-container">
+            <h1 className="main-title">Summa Solutions FED Test</h1>
+            <p className="main-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+    )
+}
+
+export default Header;
