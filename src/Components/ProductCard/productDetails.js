9ec6f40d73a66b31518c8b8ee9916b1510869bea
Emilia <m.emilia.guadalupe@gmail.com>
2021-09-07 20:28:41 -0300

    Changed the content of the array

--- src/Components/ProductCard/productDetails.js
+++ src/Components/ProductCard/productDetails.js
@@ -1,58 +1,58 @@
 export const productDetails = [
     {
-        "id": 1,
+        "id": "QWERT",
         "name": "Paragolpe Delantero Adap.",
         "img": 'https://http2.mlstatic.com/D_NQ_NP_759618-MLA40034959321_122019-O.webp',
         "price": 2.101,
         "discount": 2.626
     },
     {
-        "id": 2,
-        "name": "Paragolpe Delantero Adap.",
-        "img": 'https://http2.mlstatic.com/D_NQ_NP_759618-MLA40034959321_122019-O.webp',
-        "price": 2.101,
-        "discount": 2.626
+        "id": "YUIOP",
+        "name": "Paragolpe Delantero Clio",
+        "img": 'https://http2.mlstatic.com/D_NQ_NP_926625-MLA44100057264_112020-O.webp',
+        "price": 2.505,
+        "discount": 3.421
     },
     {
-        "id": 3,
-        "name": "Paragolpe Delantero Adap.",
-        "img": 'https://http2.mlstatic.com/D_NQ_NP_759618-MLA40034959321_122019-O.webp',
-        "price": 2.101,
-        "discount": 2.626
+        "id": "ASDFF",
+        "name": "Paragolpe Delantero 2013",
+        "img": 'https://http2.mlstatic.com/D_NQ_NP_661580-MLA31063913175_062019-O.webp',
+        "price": 2.608,
+        "discount": 2.726
     },
     {
-        "id": 4,
-        "name": "Paragolpe Delantero Adap.",
-        "img": 'https://http2.mlstatic.com/D_NQ_NP_759618-MLA40034959321_122019-O.webp',
-        "price": 2.101,
-        "discount": 2.626
+        "id": "GHJKL",
+        "name": "Paragolpe Delantero Megan",
+        "img": 'https://http2.mlstatic.com/D_NQ_NP_964444-MLA44100055547_112020-O.webp',
+        "price": 4.626,
+        "discount": 9.626
     },
     {
-        "id": 5,
-        "name": "Paragolpe Delantero Adap.",
-        "img": 'https://http2.mlstatic.com/D_NQ_NP_759618-MLA40034959321_122019-O.webp',
-        "price": 2.101,
-        "discount": 2.626
+        "id": "ZXCVB",
+        "name": "Paragolpe Delantero Clio",
+        "img": 'https://http2.mlstatic.com/D_NQ_NP_613542-MLA41105477991_032020-O.webp',
+        "price": 1.501,
+        "discount": 1.986
     },
     {
-        "id": 6,
-        "name": "Paragolpe Delantero Adap.",
-        "img": 'https://http2.mlstatic.com/D_NQ_NP_759618-MLA40034959321_122019-O.webp',
-        "price": 2.101,
-        "discount": 2.626
+        "id": "WSXCD",
+        "name": "Paragolpe Delantero Kangoo",
+        "img": 'https://http2.mlstatic.com/D_NQ_NP_706408-MLA47398459053_092021-O.webp',
+        "price": 2.908,
+        "discount": 3.626
     },
     {
-        "id": 7,
-        "name": "Paragolpe Delantero Adap.",
-        "img": 'https://http2.mlstatic.com/D_NQ_NP_759618-MLA40034959321_122019-O.webp',
-        "price": 2.101,
-        "discount": 2.626
+        "id": "KIOLP",
+        "name": "Paragolpe Delantero 2014",
+        "img": 'https://http2.mlstatic.com/D_NQ_NP_832261-MLA47397424720_092021-O.webp',
+        "price": 2.906,
+        "discount": 2.966
     },
     {
-        "id": 8,
-        "name": "Paragolpe Delantero Adap.",
+        "id": "MNHJK",
+        "name": "Paragolpe Delantero Duster",
         "img": 'https://http2.mlstatic.com/D_NQ_NP_759618-MLA40034959321_122019-O.webp',
-        "price": 2.101,
-        "discount": 2.626
+        "price": 6.606,
+        "discount": 6.826
     }
 ]
