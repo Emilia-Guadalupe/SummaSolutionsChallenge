47ce94a0501596980abc2da8e437dd0c5646e11d
Emilia <m.emilia.guadalupe@gmail.com>
2021-09-08 08:12:43 -0300

    Moving the array

--- src/Components/ProductCard/productDetails.js
+++ src/Components/ProductCard/productDetails.js
@@ -4,55 +4,63 @@ export const productDetails = [
         "name": "Paragolpe Delantero Adap.",
         "img": 'https://http2.mlstatic.com/D_NQ_NP_759618-MLA40034959321_122019-O.webp',
         "price": 2.101,
-        "discount": 2.626
+        "discount": 2.626,
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
     },
     {
         "id": "YUIOP",
         "name": "Paragolpe Delantero Clio",
         "img": 'https://http2.mlstatic.com/D_NQ_NP_926625-MLA44100057264_112020-O.webp',
         "price": 2.505,
-        "discount": 3.421
+        "discount": 3.421,
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
     },
     {
         "id": "ASDFF",
         "name": "Paragolpe Delantero 2013",
         "img": 'https://http2.mlstatic.com/D_NQ_NP_661580-MLA31063913175_062019-O.webp',
         "price": 2.608,
-        "discount": 2.726
+        "discount": 2.726,
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
     },
     {
         "id": "GHJKL",
         "name": "Paragolpe Delantero Megan",
         "img": 'https://http2.mlstatic.com/D_NQ_NP_964444-MLA44100055547_112020-O.webp',
         "price": 4.626,
-        "discount": 9.626
+        "discount": 9.626,
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
     },
     {
         "id": "ZXCVB",
         "name": "Paragolpe Delantero Clio",
         "img": 'https://http2.mlstatic.com/D_NQ_NP_613542-MLA41105477991_032020-O.webp',
         "price": 1.501,
-        "discount": 1.986
+        "discount": 1.986,
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
     },
     {
         "id": "WSXCD",
         "name": "Paragolpe Delantero Kangoo",
         "img": 'https://http2.mlstatic.com/D_NQ_NP_706408-MLA47398459053_092021-O.webp',
         "price": 2.908,
-        "discount": 3.626
+        "discount": 3.626,
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
     },
     {
         "id": "KIOLP",
         "name": "Paragolpe Delantero 2014",
         "img": 'https://http2.mlstatic.com/D_NQ_NP_832261-MLA47397424720_092021-O.webp',
         "price": 2.906,
-        "discount": 2.966
+        "discount": 2.966,
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
     },
     {
         "id": "MNHJK",
         "name": "Paragolpe Delantero Duster",
         "img": 'https://http2.mlstatic.com/D_NQ_NP_759618-MLA40034959321_122019-O.webp',
         "price": 6.606,
-        "discount": 6.826
+        "discount": 6.826,
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
     }
 ]
